185f780ab3567bfe69ece14ecb76c19ec33b0fa9
tranhtiepxuc1-creator <tranhtiepxuc1@gmail.com>
2026-06-09 17:54:50 +0700

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,13 +1,16 @@
-#import "AppDelegate.m"
+#import "AppDelegate.h"
 #import "ViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Khởi tạo cửa sổ giao diện chính cho ứng dụng độc lập trên iPhone
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     ViewController *mainVC = [[ViewController alloc] init];
     self.window.rootViewController = mainVC;
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 @end
